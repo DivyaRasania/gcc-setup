f8237c11a85c1f9522844f2e9e42d4d6d03da34e
Divya Rasania <70239770+DivyaRasania@users.noreply.github.com>
2024-01-21 15:04:55 -0500

    Update setup.ps1

--- setup.ps1
+++ setup.ps1
@@ -18,8 +18,13 @@ else
 
 Set-ExecutionPolicy Bypass -Scope Process -Force; [System.Net.ServicePointManager]::SecurityProtocol = [System.Net.ServicePointManager]::SecurityProtocol -bor 3072; iex ((New-Object System.Net.WebClient).DownloadString('https://community.chocolatey.org/install.ps1'))
 
-choco install vscode -y
 choco install mingw -y
+
+$answer = Read-Host "Do you want to install Microsoft Visual Studio Code? [Y/N]"
+if ($answer -eq "Y" -or $answer -eq "y") {
+    Write-Host "Installing Microsoft Visual Studio Code..."
+    choco install vscode -y
+}
 
 Start-Process -FilePath "%localappdata%\Programs\Microsoft VS Code\Code.exe"
 
