ea7a15b7fb29baf125d2682fbd7bdf2c1478fb6d
Divya Rasania <70239770+DivyaRasania@users.noreply.github.com>
2024-01-21 13:56:56 -0500

    Create setup.ps1

---
+++ setup.ps1
@@ -0,0 +1,7 @@
+Set-ExecutionPolicy Bypass -Scope Process -Force; [System.Net.ServicePointManager]::SecurityProtocol = [System.Net.ServicePointManager]::SecurityProtocol -bor 3072; iex ((New-Object System.Net.WebClient).DownloadString('https://community.chocolatey.org/install.ps1'))
+
+choco install vscode -y
+choco install mingw -y
+
+Start-Process -FilePath "https://marketplace.visualstudio.com/items?itemName=ms-vscode.cpptools-extension-pack"
+Start-Process -FilePath "https://marketplace.visualstudio.com/items?itemName=formulahendry.code-runner"
